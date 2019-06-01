5136722b85fc685e5d403b979acc6843180b4515
777bareman777 <45147152+777bareman777@users.noreply.github.com>
2019-06-01 21:54:28 +0900

    Update Game.cpp

--- human eat food/Game.cpp
+++ human eat food/Game.cpp
@@ -12,50 +12,50 @@ bool Human::move_check(int input)
     int tmp;
     switch (input)
     {
-        // 왼쪽
-    case 1:
-        tmp=this->x-this->distance;
-        if (tmp < 0)
-        {
-            cout << "벽에 부딪쳤습니다. 다시 입력해주세요" << endl;
-            return  false;
-        }
-        this->x-=this->distance;
-        break;
-        // 아래
-    case 2:
-        tmp=this->y+this->distance;
-        if (tmp > 9)
-        {
-            cout << "벽에 부딪쳤습니다. 다시 입력해주세요" << endl;
+            // 왼쪽
+        case 1:
+            tmp=this->x-this->distance;
+            if (tmp < 0)
+            {
+                cout << "벽에 부딪쳤습니다. 다시 입력해주세요" << endl;
+                return  false;
+            }
+            this->x-=this->distance;
+            break;
+            // 아래
+        case 2:
+            tmp=this->y+this->distance;
+            if (tmp > 9)
+            {
+                cout << "벽에 부딪쳤습니다. 다시 입력해주세요" << endl;
+                return false;
+            }
+            this->y += this->distance;
+            break;
+            // 위
+        case 3:
+            tmp=this->y-this->distance;
+            if (tmp < 0)
+            {
+                cout << "벽에 부딪쳤습니다. 다시 입력해주세요" << endl;
+                return false;
+            }
+            this->y -= this->distance;
+            break;
+            // 오른쪽
+        case 4:
+            tmp=this->x+this->distance;
+            if (tmp > 19)
+            {
+                cout << "벽에 부딪쳤습니다. 다시 입력해주세요" << endl;
+                return false;
+            }
+            this->x +=this->distance;
+            break;
+        default:
+            cout<<"이상한 값을 입력했씁니다. 다시 입력해주세요"<<endl;
             return false;
-        }
-        this->y += this->distance;
-        break;
-        // 위
-    case 3:
-        tmp=this->y-this->distance;
-        if (tmp < 0)
-        {
-            cout << "벽에 부딪쳤습니다. 다시 입력해주세요" << endl;
-            return false;
-        }
-        this->y -= this->distance;
-        break;
-        // 오른쪽
-    case 4:
-        tmp=this->x+this->distance;
-        if (tmp > 19)
-        {
-            cout << "벽에 부딪쳤습니다. 다시 입력해주세요" << endl;
-            return false;
-        }
-        this->x +=this->distance;
-        break;
-    default:
-        cout<<"이상한 값을 입력했씁니다. 다시 입력해주세요"<<endl;
-        return false;
-        break;
+            break;
     }
     return true;
 }
@@ -67,55 +67,55 @@ bool Monster::move_check(int input)
     // tmp 변수를 통해 충돌 유무를 판단후
     // 값을 변조를 시킴.
     int tmp;
-
+    
     switch (input)
     {
-        // 왼쪽
-    case 1:
-        tmp=this->x-this->distance;
-        if (tmp < 0)
-        {
-            // debug 용 프린트
-            cout << "몬스터 벽에 부딪침" << endl;
-            return  false;
-        }
-        this->x -= this->distance;
-        break;
-        // 아래
-    case 2:
-        tmp=this->y+this->distance;
-        if (tmp > 9)
-        {
-            // debug 용 프린트
-            cout << "몬스터 벽에 부딪침" << endl;
-            return false;
-        }
-        this->y += this->distance;
-        break;
-        // 위
-    case 3:
-        tmp=this->y-this->distance;
-        if (tmp < 0)
-        {
-            // debug 용 프린트
-            cout << "몬스터 벽에 부딪침" << endl;
-            return false;
-        }
-        this->y -= this->distance;
-        break;
-        // 오른쪽
-    case 4:
-        tmp=this->x+this->distance;
-        if (tmp > 19)
-        {
-            // debug 용 프린트
-            cout << "몬스터 벽에 부딪침" << endl;
-            return false;
-        }
-        this->x +=this->distance;
-        break;
-    default:
-        break;
+            // 왼쪽
+        case 1:
+            tmp=this->x-this->distance;
+            if (tmp < 0)
+            {
+                // debug 용 프린트
+                cout << "몬스터 벽에 부딪침" << endl;
+                return  false;
+            }
+            this->x -= this->distance;
+            break;
+            // 아래
+        case 2:
+            tmp=this->y+this->distance;
+            if (tmp > 9)
+            {
+                // debug 용 프린트
+                cout << "몬스터 벽에 부딪침" << endl;
+                return false;
+            }
+            this->y += this->distance;
+            break;
+            // 위
+        case 3:
+            tmp=this->y-this->distance;
+            if (tmp < 0)
+            {
+                // debug 용 프린트
+                cout << "몬스터 벽에 부딪침" << endl;
+                return false;
+            }
+            this->y -= this->distance;
+            break;
+            // 오른쪽
+        case 4:
+            tmp=this->x+this->distance;
+            if (tmp > 19)
+            {
+                // debug 용 프린트
+                cout << "몬스터 벽에 부딪침" << endl;
+                return false;
+            }
+            this->x +=this->distance;
+            break;
+        default:
+            break;
     }
     return true;
 }
@@ -127,55 +127,55 @@ bool Food::move_check(int input)
     // tmp 변수를 통해 충돌 유무를 판단후
     // 값을 변조를 시킴.
     int tmp;
-
+    
     switch (input)
     {
-        // 왼쪽
-    case 1:
-        tmp=this->x-this->distance;
-        if (tmp < 0)
-        {
-            // debug용 프린트
-            cout << "음식이 벽에 부딪침" << endl;
-            return  false;
-        }
-        this->x -= this->distance;
-        break;
-        // 아래
-    case 2:
-        tmp=this->y+this->distance;
-        if (tmp > 9)
-        {
-            // debug용 프린트
-            cout << "음식이 벽에 부딪침" << endl;
-            return false;
-        }
-        this->y += this->distance;
-        break;
-        // 위
-    case 3:
-        tmp=this->y-this->distance;
-        if (tmp < 0)
-        {
-            // debug용 프린트
-            cout << "음식이 벽에 부딪침" << endl;
-            return false;
-        }
-        this->y -= this->distance;
-        break;
-        // 오른쪽
-    case 4:
-        tmp=this->x+this->distance;
-        if (x > 19)
-        {
-            // debug용 프린트
-            cout << "음식이 벽에 부딪침" << endl;
-            return false;
-        }
-        this->x +=this->distance;
-        break;
-    default:
-        break;
+            // 왼쪽
+        case 1:
+            tmp=this->x-this->distance;
+            if (tmp < 0)
+            {
+                // debug용 프린트
+                cout << "음식이 벽에 부딪침" << endl;
+                return  false;
+            }
+            this->x -= this->distance;
+            break;
+            // 아래
+        case 2:
+            tmp=this->y+this->distance;
+            if (tmp > 9)
+            {
+                // debug용 프린트
+                cout << "음식이 벽에 부딪침" << endl;
+                return false;
+            }
+            this->y += this->distance;
+            break;
+            // 위
+        case 3:
+            tmp=this->y-this->distance;
+            if (tmp < 0)
+            {
+                // debug용 프린트
+                cout << "음식이 벽에 부딪침" << endl;
+                return false;
+            }
+            this->y -= this->distance;
+            break;
+            // 오른쪽
+        case 4:
+            tmp=this->x+this->distance;
+            if (x > 19)
+            {
+                // debug용 프린트
+                cout << "음식이 벽에 부딪침" << endl;
+                return false;
+            }
+            this->x +=this->distance;
+            break;
+        default:
+            break;
     }
     return true;
 }
@@ -236,7 +236,10 @@ void Food::move()
 {
     int ran;
     ran=rand()%2;
-
+    
+    if(count1+count2==5){
+        count1=0; count2=0;
+    }
     // 5번중 3번 제자리 체크
     if(ran==0 && count1<3){
         count1++;
@@ -244,14 +247,16 @@ void Food::move()
     }
     // 5번중 2번 움직임 체크
     else{
+        if(count2==2){
+            count1++;
+            return ;
+        }
         count2++;
         do{
             ran=rand()%4+1;
         } while(!move_check(ran));
     }
-    if(count1+count2==5){
-        count1=0; count2=0;
-    }
+    
 }
 
 // Food 형태를 리턴함.
